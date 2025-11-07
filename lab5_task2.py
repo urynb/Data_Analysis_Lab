bf392a84baa6e60bd84f399b21af51324ba25879
Upmanyu <98857897+urynb@users.noreply.github.com>
2025-11-07 17:54:18 +0900

    Update lab5_task2.py

--- lab5_task2.py
+++ lab5_task2.py
@@ -14,6 +14,7 @@ df['time_s'] = (df['timestamp'] - df['timestamp'].iloc[0]) / 1000.0
 # magnitude is the square root of X squared + Y squared + Z squared
 df['mag'] = np.sqrt(df['x']**2 + df['y']**2 + df['z']**2)
 
+# Edit this
 # input a threshold based on the plot you just saw
 # this represents "how much" deviation from the baseline (9.8 m/s^2, or the force of "resting gravity")
 # you think represents movement 
@@ -24,7 +25,7 @@ threshold = 0
 # when Magnitude > Threshold 
 df['state'] = np.where(df['dev'] > threshold, 'Moving', 'Still')
 
+# Add your code here
 # plot a pie chart of state proportions using plt
-# you may need to change this code
 
 
